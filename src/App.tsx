a35dafd13c1d54c684a12338217b396b4b4415eb
Johann Dreo <johann.dreo@pasteur.fr>
2021-07-29 16:37:02 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Oncoview from './components/Oncoview/Oncoview'
 
 function App() {
   return (
@@ -7,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Oncodash.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Oncoview />
       </header>
     </div>
   );
