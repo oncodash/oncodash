48aca6431d6474a7377c77b25b3eca3f7e3d8c16
Okunator <oskari.lehtonen@helsinki.fi>
2021-12-20 13:55:03 +0200

    fix: initial steps to integrate front and backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from core import views
@@ -8,5 +8,10 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/explainer/', include('explainer.api.urls')),
-    path('', views.home)
 ] + staticfiles_urlpatterns()
+
+
+# django does not handle http errors, front-end needs to do this 
+urlpatterns += [
+    re_path(r'(?P<path>.*)$', views.FrontendRenderView.as_view(), name='home')
+]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,11 +1,17 @@
+from django.views.generic import View
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 
-def home(request):
-    """
-    Place holder view
+class FrontendRenderView(View):
+    """Render view for rendering the front-end content"""
+    
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """GET method to render front-end view
 
-    :param request: request object
-    :return: page rendering to be displayed
-    """
-    return render(request, 'index.html')
+        :param request: request object
+        :param type: class: `HttpRequest`
+        :return: page rendering to be displayed
+        :rtype: HttpResponse
+        """
+        return render(request, "index.html")
