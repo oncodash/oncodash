39bd771bd7b07d78ea4e5480da17f8a7f4384e62
Simon Malesys <simon.malesys@pasteur.fr>
2024-02-01 18:03:54 +0100

    Fix tabs style

--- frontend/src/components/AppTabs.vue
+++ frontend/src/components/AppTabs.vue
@@ -44,7 +44,6 @@ onMounted(() => {
 <style scoped>
 .tabs-navigation {
   align-items: center;
-  border-bottom: 1px solid var(--grey);
   display: flex;
   flex-flow: row wrap;
 }
@@ -53,7 +52,6 @@ onMounted(() => {
   background-color: var(--white);
   border-radius: 20px 20px 0 0;
   border: 1px solid var(--grey);
-  box-sizing: border-box;
   color: var(--black);
   flex-grow: 1;
   padding: var(--spacing);
