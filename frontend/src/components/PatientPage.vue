f68c515d5954fc8ce1021380a255105e95602903
Simon Malesys <simon.malesys@pasteur.fr>
2024-02-01 11:58:43 +0100

    Put patient summary in its own component

--- frontend/src/components/PatientPage.vue
+++ frontend/src/components/PatientPage.vue
@@ -1,58 +1,7 @@
 <template>
   <h1>PATIENT {{ id }}</h1>
-  <section class="patient-summary">
-    <img src="../assets/patient.svg" alt="" srcset="">
-    <div>
-      <div>
-        Patient ID -
-        <span class="value">{{ patient.patient_id }}</span>
-      </div>
-      <div>
-        Cohort -
-        <span class="value">{{ patient.cohort_code }}</span>
-      </div>
-      <div>
-        Age -
-        <span class="value">{{ patient.age_at_diagnosis }}</span>
-      </div>
-      <div>
-        Stage -
-        <span class="value">{{ patient.stage }}</span>
-      </div>
-    </div>
-    <div>
-      <div>
-        Status -
-        <span class="value">{{ patientStatus }}</span>
-      </div>
-      <div>
-        Current phase -
-        <span class="value">{{ patient.current_treatment_phase }}</span>
-      </div>
-      <div>
-        Progression -
-        <span class="value">{{ patient.progression }}</span>
-      </div>
-    </div>
-    <div>
-      <div>
-        PFS -
-        <span class="value">{{ patient.paired_fresh_samples_available }}</span>
-      </div>
-      <div>
-        PFI -
-        <span class="value">{{ patient.platinum_free_interval }}</span>
-      </div>
-      <div>
-        Days to death -
-        <span class="value">{{ patient.days_to_death }}</span>
-      </div>
-      <div>
-        Follow-up time -
-        <span class="value">{{ patient.followup_time }}</span>
-      </div>
-    </div>
-  </section>
+
+  <PatientSummary :patient="patient"></PatientSummary>
 
   <section class="patient-data">
     <AppTabs>
@@ -65,10 +14,11 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import api from '../api'
 import AppTabs from "./AppTabs.vue"
 import AppTabsPanel from './AppTabsPanel.vue'
+import PatientSummary from "./PatientSummary.vue";
 
 const props = defineProps<{
   id: string
@@ -84,35 +34,9 @@ onMounted(async () => {
 })
 
 const patient: any = ref({})
-const patientStatus = computed(() => {
-  if (patient.value.survival === 'True') return 'Alive'
-  else if (patient.value.survival === 'False') return 'Deceased'
-  else 'Unknown'
-})
 </script>
 
 <style scoped>
-.patient-summary {
-  background-color: var(--white);
-  border-radius: var(--radius);
-  width: max(500px, 70%);
-  margin: auto;
-  padding: var(--spacing);
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-}
-
-.patient-summary > div {
-  display: flex;
-  flex-flow: column wrap;
-  gap: 5px;
-}
-
-.patient-summary .value {
-  font-weight: bold;
-}
-
 .patient-data {
   margin: var(--spacing);
 }
---
+++ frontend/src/components/PatientSummary.vue
@@ -0,0 +1,95 @@
+<template>
+  <section class="patient-summary">
+    <img src="../assets/patient.svg">
+
+    <div>
+      <div>
+        Patient ID -
+        <span class="value">{{ patient.patient_id }}</span>
+      </div>
+      <div>
+        Cohort -
+        <span class="value">{{ patient.cohort_code }}</span>
+      </div>
+      <div>
+        Age -
+        <span class="value">{{ patient.age_at_diagnosis }}</span>
+      </div>
+      <div>
+        Stage -
+        <span class="value">{{ patient.stage }}</span>
+      </div>
+    </div>
+
+    <div>
+      <div>
+        Status -
+        <span class="value">{{ patientStatus }}</span>
+      </div>
+      <div>
+        Current phase -
+        <span class="value">{{ patient.current_treatment_phase }}</span>
+      </div>
+      <div>
+        Progression -
+        <span class="value">{{ patient.progression }}</span>
+      </div>
+    </div>
+
+    <div>
+      <div>
+        PFS -
+        <span class="value">{{ patient.paired_fresh_samples_available }}</span>
+      </div>
+      <div>
+        PFI -
+        <span class="value">{{ patient.platinum_free_interval }}</span>
+      </div>
+      <div>
+        Days to death -
+        <span class="value">{{ patient.days_to_death }}</span>
+      </div>
+      <div>
+        Follow-up time -
+        <span class="value">{{ patient.followup_time }}</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  patient: any
+}>()
+
+const patientStatus = computed(() => {
+  if (props.patient.survival === 'True') return 'Alive'
+  else if (props.patient.survival === 'False') return 'Deceased'
+  else 'Unknown'
+})
+</script>
+
+<style scoped>
+.patient-summary {
+  align-items: center;
+  background-color: var(--white);
+  border-radius: var(--radius);
+  display: flex;
+  justify-content: space-around;
+  margin: auto;
+  padding: var(--spacing);
+  width: max(500px, 70%);
+}
+
+.patient-summary > div {
+  display: flex;
+  flex-flow: column wrap;
+  gap: 5px;
+}
+
+.patient-summary .value {
+  font-weight: bold;
+}
+</style>
