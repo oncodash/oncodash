52bf978adef1f80bda98bcefe14d34f44e3f4aa5
Johann Dreo <johann.dreo@pasteur.fr>
2024-04-11 23:05:59 +0200

    feat(anon): refactor and clean

--- backend/anonymize_sqlite.py
+++ backend/anonymize_sqlite.py
@@ -5,35 +5,34 @@ import string
 import random
 import logging
 
-def column_to_tables(targets, cur):
+def unfold(list_of_tuples, sep=", "):
+    return sep.join(v[0] for v in list_of_tuples)
 
+
+def column_to_tables(targets, cur):
     cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
     tables = cur.fetchall()
-    logging.debug(tables)
-
 
     col2tab = {}
     for t in tables:
         table = t[0]
-        logging.debug(table)
+        logging.debug(f"\t{table}")
 
         q=f"SELECT p.name as columnName FROM sqlite_master m LEFT OUTER JOIN pragma_table_info((m.name)) p on m.name <> p.name WHERE m.name == \"{table}\" ORDER BY m.name, columnName;"
-        # logging.debug(q)
         cur.execute(q)
 
         columns = cur.fetchall()
-        logging.debug(columns)
 
         for c in columns:
             column = c[0]
             if column in targets:
+                logging.debug(f"\t\t{column}")
                 if column in col2tab:
                     col2tab[column].append(table)
                 else:
                     col2tab[column] = [table]
 
     return col2tab
-
 
 
 def find_cohort_code(value):
@@ -68,10 +67,43 @@ def find_cohort_codes(col2tab, cur):
     return cohort_codes
 
 
+def anonymize_value(value, anon_map):
+    assert(";" not in value)
+    value = re.sub("_DNA[1-2]*$", "", value)
+    m = re.search("^([A-Z]{1,2}[0-9]{3})", value)
+    if m:
+        code = m.group(1)
+        return re.sub(code, anon_map[code], value)
+    else:
+        return None
+
+def anonymize_all(col2tab, cur):
+    for column in col2tab:
+        logging.info(f"\t{column}")
+        for table in col2tab[column]:
+            # Long entries first, to avoid partial replacement.
+            cur.execute(f"SELECT DISTINCT {column} FROM {table} ORDER BY length({column}) DESC;")
+            values = cur.fetchall()
+            logging.info(f"\t\t{table}")
+            for v in values:
+                value = v[0]
+                if ";" in value:
+                    vals = []
+                    for val in value.split(";"):
+                        new = anonymize_value(val, anon_map)
+                        vals.append(new)
+                    new_val = ";".join(vals)
+                else:
+                    new_val = anonymize_value(value, anon_map)
+
+                if value and new_val:
+                    # logging.debug(f"\t{value}\t=>\t{new_val}")
+                    cur.execute(f"UPDATE {table} SET {column} = REPLACE({column}, '{value}', '{new_val}');")
+
+
 if __name__ == "__main__":
     import argparse
     import sqlite3
-
 
     do = argparse.ArgumentParser(
         prog='Oncodash SQLite3 database Anonymizer',
@@ -82,6 +114,9 @@ if __name__ == "__main__":
         choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
         help="Configure the log level (default: %(default)s).",
     )
+
+    do.add_argument('-t', '--targets', nargs='+', default=["cohort_code", "sample_id", "samples", "sample"],
+        help="Columns names in which to anonymize cohort codes.")
 
     do.add_argument("filename")
 
@@ -100,13 +135,16 @@ if __name__ == "__main__":
     logging.debug("Connection OK")
 
     logging.info("Find out which tables contains targetable columns...")
-    targets = ["cohort_code", "sample_id", "samples", "sample"]
+    targets = asked.targets
     col2tab = column_to_tables(targets, cur)
-    logging.info(col2tab)
+    logging.debug("Columns to tables:")
+    for col in col2tab:
+        logging.debug(f"\t{col}")
+        for tab in col2tab[col]:
+            logging.debug(f"\t\t{tab}")
 
     logging.info("Find out cohort codes...")
     cohort_codes = find_cohort_codes(col2tab, cur)
-    # logging.debug(cohort_codes)
 
     logging.info("Prepare anonymization mapping...")
     anon_map = {}
@@ -117,38 +155,7 @@ if __name__ == "__main__":
         logging.debug(f"\t{cc}\t=>\t{anon_map[cc]}")
 
     logging.info("Compute anonymization...")
-
-    def anonymize(value, anon_map):
-        assert(";" not in value)
-        value = re.sub("_DNA[1-2]*$", "", value)
-        m = re.search("^([A-Z]{1,2}[0-9]{3})", value)
-        if m:
-            code = m.group(1)
-            return re.sub(code, anon_map[code], value)
-        else:
-            return None
-
-    for column in col2tab:
-        for table in col2tab[column]:
-            # Long entries first, to avoid partial replacement.
-            cur.execute(f"SELECT DISTINCT {column} FROM {table} ORDER BY length({column}) DESC;")
-            values = cur.fetchall()
-            # logging.debug(values)
-            for v in values:
-                value = v[0]
-                if ";" in value:
-                    vals = []
-                    for val in value.split(";"):
-                        new = anonymize(val, anon_map)
-                        vals.append(new)
-                    new_val = ";".join(vals)
-                else:
-                    new_val = anonymize(value, anon_map)
-
-                if value and new_val:
-                    # logging.debug(f"\t{value}\t=>\t{new_val}")
-                    cur.execute(f"UPDATE {table} SET {column} = REPLACE({column}, '{value}', '{new_val}');")
-                    # con.commit()
+    anonymize_all(col2tab, cur)
 
     logging.info("Apply anonymization...")
     con.commit()
