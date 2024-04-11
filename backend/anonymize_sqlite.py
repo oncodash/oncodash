d6d9de1ac1ccdc1b29b7763d3df449954282b73b
Johann Dreo <johann.dreo@pasteur.fr>
2024-04-11 21:02:50 +0200

    feat(sqlite): find cohort codes in data

--- backend/anonymize_sqlite.py
+++ backend/anonymize_sqlite.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+
+import re
 import logging
 
 def column_to_tables(targets, cur):
@@ -29,6 +31,38 @@ def column_to_tables(targets, cur):
                     col2tab[column] = [table]
 
     return col2tab
+
+
+def find_cohort_codes(col2tab, cur):
+    queries = []
+    for column in col2tab:
+        for table in col2tab[column]:
+            queries.append(f"SELECT {column} FROM {table}")
+    query = " UNION ".join(queries)
+
+    def find_cohort_code(value):
+        m = re.search("^([A-Z][0-9]{3})_\w*.*$", value)
+        if m:
+            return m.group(1)
+        elif re.match("^[A-Z][0-9]{3}$", value):
+            return value
+        else:
+            return None
+
+    cohort_codes = set()
+    for v in cur.execute(query):
+        value = v[0]
+        if ";" in value: # Multiple values
+            for val in value.split(";"):
+                cc = find_cohort_code(val)
+                if cc:
+                    cohort_codes.add(cc)
+        else:
+            cc = find_cohort_code(value)
+            if cc:
+                cohort_codes.add(cc)
+
+    return cohort_codes
 
 
 if __name__ == "__main__":
@@ -66,6 +100,10 @@ if __name__ == "__main__":
     targets = ["cohort_code", "sample_id", "samples", "sample"]
     col2tab = column_to_tables(targets, cur)
     logging.info(col2tab)
+
+    logging.info("Find out cohort codes...")
+    cohort_codes = find_cohort_codes(col2tab, cur)
+    logging.debug(cohort_codes)
 
     logging.debug("Close database connection")
     con.close()
