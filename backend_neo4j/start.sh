8816dd441bb41cbe0b49778b6981be0cc0352d10
Johann Dreo <johann.dreo@pasteur.fr>
2026-04-09 08:54:53 +0200

    feat(backend): init flask API

---
+++ backend_neo4j/start.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+
+echo "Starting Neo4j database..." >&2
+
+case "$(uname)" in
+    FreeBSD)   OS=FreeBSD ;;
+    DragonFly) OS=FreeBSD ;;
+    OpenBSD)   OS=OpenBSD ;;
+    Darwin)    OS=Darwin  ;;
+    SunOS)     OS=SunOS   ;;
+    *)         OS=Linux   ;;
+esac
+
+echo "Running under $OS" >&2
+if [[ "$OS" == "Linux" ]] ; then
+    # When using Neo4j installed on system (like Ubuntu's packaged version),
+    # the current directory must be writable by user "neo4j",
+    # and all parent directories must be executable by "other".
+    # Every interaction with the database must be done by user "neo4j",
+    # and the import will try to write reports in the current directory.
+    NEO_USER="sudo -u neo4j"
+    # export JAVA_HOME="/usr/lib/jvm/java-21-openjdk-amd64"
+else
+    NEO_USER=""
+fi
+
+${NEO_USER} neo4j-admin server status
+
+if [[ $? ]] ; then
+    echo "Access Neo4j browser at: http://localhost:7474" >&2
+else
+    neo_version=$(neo4j-admin --version | cut -d. -f 1)
+    if [[ "$neo_version" -eq 4 ]]; then
+        server="${NEO_USER} neo4j"
+    else
+        server="${NEO_USER} neo4j-admin server"
+    fi
+    $server start
+fi
+
+echo "Send a test query..." >&2
+${NEO_USER} cypher-shell --username neo4j --database oncodash --password "$(cat neo4j.pass)" "MATCH (p:Patient) RETURN p LIMIT 20;"
+
+
+echo "Starting Flask API..." >&2
+export FLASK_APP="app.py"
+uv run flask --app app run --debug
+
