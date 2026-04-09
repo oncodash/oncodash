69aad6281a006e819febbaf6d661c0a328e3ffc2
Johann Dreo <johann.dreo@pasteur.fr>
2026-04-09 10:23:01 +0200

    feat(neo4j): patients test

--- backend_neo4j/app.py
+++ backend_neo4j/app.py
@@ -1,7 +1,14 @@
-
 import flask
+import neo4j
+import logging
 
 app = flask.Flask(__name__)
+
+neo4j_uri = "neo4j://localhost:7687"
+
+with open("neo4j.pass") as fd:
+    neo4j_passwd = fd.readline().strip()
+neo4j_auth = ("neo4j", neo4j_passwd)
 
 @app.route("/ping")
 def ping():
@@ -15,6 +22,24 @@ def ping():
         "version": "0.1.0",
     }
     return flask.jsonify({"info": api_info})
+
+
+@app.route("/patients")
+def patients():
+    with neo4j.GraphDatabase.driver(neo4j_uri, auth=neo4j_auth) as db:
+        records, _, _ = db.execute_query(
+            "MATCH (p:Patient) RETURN ALL * LIMIT 5",
+            name="neo4j", database_ = "oncodash")
+    patients = []
+    app.logger.debug(f"{len(records)} records")
+    for r in records:
+        app.logger.debug(r)
+        rp = r["p"]
+        patient = {}
+        for key,val in rp._properties.items():
+            patient[key] = val
+        patients.append(patient)
+    return flask.jsonify(patients)
 
 
 if __name__ == "__main__":
