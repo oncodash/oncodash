f450457f64077b7535c86fdf837f53ebfdca8039
Johann Dreo <johann.dreo@pasteur.fr>
2026-04-21 17:19:19 +0200

    feat(frontend): label HRD status (SBS3)

--- frontend/src/components/PatientClinical.vue
+++ frontend/src/components/PatientClinical.vue
@@ -20,7 +20,7 @@
 
     <div>
       <div class="title">BASIC GENETICS</div>
-      <PatientField field="HRD status" :value="patient.hr_signature_per_patient" />
+      <PatientField field="HRD status (SBS3)" :value="patient.hr_signature_per_patient" />
       <PatientField field="BRCA mutation" :value="patient.brca_mutation_status" />
       <PatientField field="Germ line pathogenic variants" :value="patient.germline_pathogenic_variant" />
     </div>
